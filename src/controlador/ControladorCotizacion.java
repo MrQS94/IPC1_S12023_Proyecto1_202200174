a8aa404b14757ffae456ae8dfd19ce73aa523259
MrQS94 <quezale@outlook.com>
2023-03-09 22:58:18 -0600

    1.0

--- src/controlador/ControladorCotizacion.java
+++ src/controlador/ControladorCotizacion.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
@@ -109,7 +109,6 @@ public class ControladorCotizacion implements ItemListener, KeyListener, ActionL
                 reg = listReg.get(i).getNombre();
             }
         }
-
         return reg;
     }
 
